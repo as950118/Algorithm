25c1c305f7ec8735e959fc44d5561236b17524dd
as950118 <na_qa@icloud.com>
2018-06-23 23:27:13 +0900

    Algorithm

--- python/DP_11053.py
+++ python/DP_11053.py
@@ -8,15 +8,15 @@ ret = 0
 
 def func(n, i):
     global ret
+    dp[i] = 1;
     if n<i:
         return 0
-    if dp[i-1]<arr2[i]:
-        dp[i] = arr2[i]
-        ret+=1
-    else:
-        dp[i] = dp[i-1]
-
+    for j in range(1, i):
+        if arr2[i]>arr2[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+    ret = max(ret, dp[i])
     func(n, i+1)
+
 
 
 N = int(input())
@@ -25,7 +25,6 @@ arr = input().split()
 
 for i in range(0, N):
     arr2[i+1] = int(arr[i])
-
 
 func(N, 1)
 print(ret)
