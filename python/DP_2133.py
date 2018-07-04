56009834333e43cffccd29c9087c3a66d358a254
as950118 <na_qa@icloud.com>
2018-07-04 19:24:14 +0900

    algorithm

--- python/DP_2133.py
+++ python/DP_2133.py
@@ -2,24 +2,12 @@ import sys
 sys.setrecursionlimit(1000000000)
 debug = 1
 while debug:
-    n, m = input().split()
-    n = int(n)
-    m = int(m)
-    arr = [[0]*m for i in range(n)]
-    dp = [[0]*m for i in range(n)]
-    for i in range(0, n):
-        arr[i] = input().split()
-        for j in range(0, m):
-            arr[i][j] = int(arr[i][j])
-
-    dp[0][0] = arr[0][0]
-    for i in range(1, m):
-        dp[0][i] = dp[0][i-1] + arr[0][i]
-    for i in range(1, n):
-        dp[i][0] = dp[i-1][0] + arr[i][0]
-
-    for i in range(1, n):
-        for j in range(1, m):
-            dp[i][j] = max(dp[i-1][j], dp[i][j-1])+ arr[i][j]
-    print(dp[n-1][m-1])
+    n = int(input())
+    dp = [n]*10001
+    '''
+    2 = 3
+    4 = 3*3 - 2 + 2*5 - 1 = 18
+    6 = 3*3*3-2 + 2*(18*3 - 2) =
+    8 =
+    '''
     debug = 1
