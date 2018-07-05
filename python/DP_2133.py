79fc81a60f4213f7c25a26a4edaf6a0490ec0032
as950118 <na_qa@icloud.com>
2018-07-05 17:19:43 +0900

    Algorithm

--- python/DP_2133.py
+++ python/DP_2133.py
@@ -3,11 +3,14 @@ sys.setrecursionlimit(1000000000)
 debug = 1
 while debug:
     n = int(input())
-    dp = [n]*10001
+    dp = [0]*10001
     '''
     2 = 3
-    4 = 3*3 - 2 + 2*5 - 1 = 18
-    6 = 3*3*3-2 + 2*(18*3 - 2) =
+    4 = 3*3 + 2*5 - 1 = 18
+    6 = 222 or 24 3*3*3 + 3*18 = 3*27 = 81
     8 =
     '''
+    for i in range(2, n+1, 2):
+        dp[i] = (i-1)*(3**(i-1))
+    print(dp[n])
     debug = 1
