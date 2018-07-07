cd5c31d6dcfe31655ce8acab2928e1a03f6711aa
as950118 <na_qa@icloud.com>
2018-07-07 22:40:32 +0900

    Algorithm

--- python/DP_1699.py
+++ python/DP_1699.py
@@ -1,24 +1,26 @@
 import sys
 sys.setrecursionlimit(1000000000)
 debug = 1
+dp = [0]*10000001
+'''
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22
+1 2 3 1 2 3 4 2 1 2  3  3  2  3  4  1  2  2  3  2  3  3
+'''
 def func(n, i):
     global ret
     while 1:
-        if i**2==n:
-            ret+=1
-            return 0
-        if i**2 > n:
-            i-=1
-            n = n-i**2
-            ret+=1
-            func(n, 1)
-            return 0
+        if n==i**2:
+            ret +=1
+            break
+        if n>i**2:
+            max = n/i**2
+            if max:
+            func(n, i+1)
+            break
         else:
             i+=1
 while debug:
     ret=0
     n = int(input())
-    func(n, 1)
-    print(ret)
-
+    print(dp[n])
     debug=1
