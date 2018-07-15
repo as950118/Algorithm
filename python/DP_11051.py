232fa18cba6c3a8f4750d904b9de061b82b437ae
as950118 <na_qa@icloud.com>
2018-07-15 15:51:49 +0900

    Algorithm

--- python/DP_11051.py
+++ python/DP_11051.py
@@ -7,15 +7,14 @@ if n/2 > n-k:
 ret = 0
 def func():
     ret = 1
-    b = 1
-    c = 1
+    mod = 1
     for i in range(k):
         ret = ret * (n-i)
         if k-i>0:
-            b = b * (k-i)
-            if ret % b == 0:
-                ret = ret // b
-                b = 1
+            mod = mod * (k-i)
+            if ret % mod == 0:
+                ret = ret // mod
+                mod = 1
     if ret >=10007:
         ret = ret % 10007
     return ret
