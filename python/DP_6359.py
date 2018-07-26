1c766ba6b11426f9240e9b8eb7c45fb0e6171a91
as950118 <na_qa@icloud.com>
2018-07-26 23:58:39 +0900

    Algorithm

--- python/DP_6359.py
+++ python/DP_6359.py
@@ -2,23 +2,77 @@ import sys
 sys.setrecursionlimit(1000000000)
 from collections import deque
 '''
-소수는 1
-짝수는 2
-홀수는 3
+자기자신을 제외한 소수
+소수는 1 == 2번이므로 닫힘
+짝수갯수는 2 == 닫힘
+홀수갯수는 3 == 열림
 '''
+arr = [1]*(101)
+arr[0], arr[1] = -1, 3
+arr[2], arr[3] = 1, 1
 t = int(input())
-arr = deque([0]*(n+1))
-arr[1],arr[2],arr[3] = 1,1,1
-primenum = 3
-mod = deque([0]*(n+1))
+def func(i, cur, last):
+    #print(cur, arr.index(1, last+1))
+    if cur%arr.index(1, last+1) == 0:
+        cur = cur/arr.index(1, last+1)
+        if arr[i] == 2:
+            arr[i] =3
+        else:
+            arr[i] = 2
+    else:
+        last = arr.index(1, last+1)
+        if last == lastprimenum:
+            if arr[i] == 1:
+                return 1
+            else:
+                return 0
+    print(i, cur, last, lastprimenum)
+    #print(arr)
+    func(i, cur, last)
+
 while t:
+    ret = 0
     n = int(input())
-    m = int(input())
+    lastprimenum = 3
     for i in range(4, n+1):
-        if i%2 == 0
-            if arr[i%2] % 2 ==0
+        j = func(i,i, 0)
+        if j ==1:
+            lastprimenum = i
+            if i==5:
+                print("SEXADSASDASDASDASDASD")
+        else:
+            pass
+
+    ret = 0
+
+    for i in range(1, n+1):
+        if arr[i] == 3:
+            ret +=1
+    print(ret)
+    for i in range(0, 90, 10):
+        print(arr[i+1:i+11])
+'''
+    for i in range(4, n+1):
+        if i%2 == 0:
+            if arr[i%2] % 2 ==0:
                 arr[i] = 3
         else:
-            pass:
+            for j in range(primenum):
+                if i%arr.index(1)==0:
+                    if arr[i] == 0:
+                        arr[i] = 2
 
-        t -= 1
+                    elif arr[i] == 1:
+                        arr[i] = 2
+                        primenum-=1
+                    else:
+                        arr[i] = 3:
+
+                else:
+                    arr[i] = 1
+                    primenum+=1
+    for i in range(1, n+1):
+        if arr[i]%2 ==1:
+            ret +=1
+    print(ret)
+'''
