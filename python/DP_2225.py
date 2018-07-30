181774ad98e296580c378226aa01788f13ab38c3
as950118 <na_qa@icloud.com>
2018-07-30 13:32:01 +0900

    Algorithm

--- python/DP_2225.py
+++ python/DP_2225.py
@@ -1,6 +1,5 @@
 import sys
 import math
-#import thread
 sys.setrecursionlimit(1000000000)
 from collections import deque
 '''
@@ -11,17 +10,16 @@ n을 k개로 만들어야하므로
 그러므로 1 + n + n*n-1/2 + n*n-1*n-2/2*3..를 계산하면된다.
 '''
 n, k = map(int, input().split())
-dp_a = [0]*(k+1)
-dp_b = [0]*(k+1)
-dp = [0]*(k+1)
-dp[1] = 1
+dp_a = [1]*(k+1)
+dp_b = [1]*(k+1)
 dp_a[1] = 1
 dp_b[1] = 1
+
+dp = [1]*(k+1)
+
 sum = 1
-for i in range(2, k+1):
-    #dp_a[i] = dp_a[i-1] * (n+i-1)
-    #dp_b[i] = dp_b[i-1] * i-1
-    dp[i] = (dp[i-1]*(n+i-1))/i-1
-    #sum = sum + (dp_a[i] // dp_b[i])
-print(int(sum)%1000000000)
-print(dp[k])
+for i in range(1, k):
+    dp_a[i] = dp_a[i-1] * (n+i-1)
+    dp_b[i] = dp_b[i-1] * (i)
+    sum = sum + (dp_a[i] // dp_b[i])
+print(int(sum%1000000000))
