03b0285b8c1ced5ad4e7e8fefd512624f2ddfa0e
as950118 <na_qa@icloud.com>
2018-10-29 03:24:08 +0900

    Algorithm

--- python/DP_11053.py
+++ python/DP_11053.py
@@ -1,30 +1,26 @@
 import sys
 sys.setrecursionlimit(1000000000)
+from collections import deque
 
-dp = [0]*1000000
-arr = [0]*1000000
-arr2 = [0]*1000000
+n = int(sys.stdin.readline())
+arr = deque(map(int, sys.stdin.readline().split()))
+arr.appendleft(0)
+
+dp = deque([0]*(n+1))
+
 ret = 0
 
-def func(n, i):
+def func(i):
     global ret
-    dp[i] = 1;
     if n<i:
         return 0
-    for j in range(1, i):
-        if arr2[i]>arr2[j]:
-            dp[i] = max(dp[i], dp[j] + 1)
+    dp[i] = 1
+
+    for j in range(1,i):
+        if arr[i]>arr[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+    func(i+1)
+
     ret = max(ret, dp[i])
-    func(n, i+1)
-
-
-
-N = int(input())
-
-arr = input().split()
-
-for i in range(0, N):
-    arr2[i+1] = int(arr[i])
-
-func(N, 1)
-print(ret)
+    return ret
+print(func(1))
