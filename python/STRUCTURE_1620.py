b73bd3495e52f59a7ee86a0c6d7d4f7a55432169
as950118 <na_qa@icloud.com>
2019-02-11 02:05:11 +0900

    Algorithm

--- python/STRUCTURE_1620.py
+++ python/STRUCTURE_1620.py
@@ -1,14 +1,14 @@
 import sys
 n, m = map(int ,sys.stdin.readline().strip().split())
-num = [None for i in range(n)]
-name = {}
+num = [None for i in range(n)] #순서를 기준으로
+name = {} #이름을 기준으로
 for i in range(n):
-    Input = sys.stdin.readline().strip()
+    Input = sys.stdin.readline().strip() #input과 sys.stdin.radline 은 속도차이가 상당함
     num[i]= Input
     name[Input] = i
 for i in range(m):
     Input = sys.stdin.readline().strip()
-    try:
+    try: #입력이 숫자일 경우
         print(num[int(Input)-1])
-    except:
+    except: #입력이 숫자가 아닐경우
         print(1+name[Input])
