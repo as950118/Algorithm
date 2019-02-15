854b3ce3318ecbda69c7562481bf768251008df1
as950118 <na_qa@icloud.com>
2019-02-16 01:05:25 +0900

    Algorithm

--- python/MST_1922.py
+++ python/MST_1922.py
@@ -1,22 +1,28 @@
 import sys
-def input():
-    return sys.stdin.readline().strip()
+input = lambda : sys.stdin.readline().strip()
 n = int(input())
 m = int(input())
-edge = [[] for i in range(n)]
-sel = [0 for i in range(n)]
+edge = [[sys.maxsize for i in range(n)] for i in range(n)]
 for i in range(m):
     a,b,c=map(int, input().split())
-    edge[a-1].append((b-1,c))
-    edge[b-1].append((a-1,c))
+    edge[a-1][b-1]= c
+    edge[b-1][a-1]= c
 
-mst = []
-sel[0] = 0
-for i in range(len(edge[0])):
-    x,y = edge[0][i].pop(0)
-    mst.append((-y, x))
-ret = 0
-start = 0
-end = 0
-while mst:
-    start, end = mst.pop(0)
+def prim():
+    key = [sys.maxsize for i in range(n)]
+    parent = [None for i in range(n)]
+    key[0] = 0
+    mstset = [0 for i in range(n)]
+    for i in range(n):
+        min_d = sys.maxsize
+        min_n = None
+        for j in range(n):
+            if key[j]<min_d and not mstset[j]:
+                min_d = key[j]
+                min_n = j
+        mstset[min_n] = 1
+        for k in range(n):
+            if edge[min_n][k] and not mstset[k] and key[k] > edge[min_n][k]:
+                key[k] = edge[min_n][k]
+    return sum(key)
+print(prim())
