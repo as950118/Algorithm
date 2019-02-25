3443b83b074c1cf9a6bc752148e767e346293f6c
as950118 <na_qa@icloud.com>
2019-02-25 18:59:17 +0900

    Algorithm

--- python/BELLMAN-FORD/BELLMAN-FORD_11657.py
+++ python/BELLMAN-FORD/BELLMAN-FORD_11657.py
@@ -3,30 +3,36 @@ inf = sys.maxsize
 input = lambda:sys.stdin.readline().strip()
 
 N, M = map(int, input().split())
-graph = [[inf for i in range(N)] for i in range(N)]
+edge = [[] for i in range(N)]
 for i in range(M):
     a,b,c = map(int, input().split())
-    graph[a-1][b-1] = c
+    edge[a-1].append((b-1, c))
 
-def bellmanford(graph, root):
+def bellmanford(edge, root):
     dist = [inf for i in range(N)]
-    pre = [None for i in range(N)]
+    #pre = [None for i in range(N)]
     dist[root] = 0
 
     #Relax edge
     for i in range(N-1):
         for n in range(N):
-            for next_n in range(N):
-                if dist[next_n] > dist[n] + graph[n][next_n]:
-                    dist[next_n] = dist[n] + graph[n][next_n]
-                    pre[next_n] = n
+            for next_n, next_d in edge[n]:
+                if dist[n] != inf: #예외처리 중요
+                    if dist[next_n] > dist[n] + next_d:
+                        dist[next_n] = dist[n] + next_d
+                        #pre[next_n] = n
 
     #Check negative cycle
     for n in range(N):
-        for next_n in range(N):
-            if dist[next_n] > dist[n] + graph[n][next_n]:
-                return -1 #Negative cycle, 무한대로 줄어듬
-                break
+        for next_n, next_d in edge[n]:
+            if dist[n] != inf: #예외처리 중요
+                if dist[next_n] > dist[n] + next_d:
+                    return [-1] #Negative cycle, 무한대로 줄어듬
+                    break
 
-    return dist
-temp = bellmanford(graph,0)  
+    return dist[1:]
+for i in bellmanford(edge, 0):
+    if i==inf:
+        print(-1)
+    else:
+        print(i)
--- python/BELLMAN-FORD/bellmanford.py
+++ python/BELLMAN-FORD/bellmanford.py
@@ -8,7 +8,14 @@ graph =[
 ]
 N = len(graph)
 
-def bellmanford(graph, root):
+#Making edge
+edge = [[] for i in range(N)]
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] != inf:
+            edge[i].append((j, graph[i][j]))
+
+def bellmanford(edge, root):
     dist = [inf for i in range(N)]
     pre = [None for i in range(N)]
     dist[root] = 0
@@ -16,17 +23,19 @@ def bellmanford(graph, root):
     #Relax edge
     for i in range(N-1):
         for n in range(N):
-            for next_n in range(N):
-                if dist[next_n] > dist[n] + graph[n][next_n]:
-                    dist[next_n] = dist[n] + graph[n][next_n]
-                    pre[next_n] = n
+            for next_n, next_d in edge[n]:
+                if dist[n] != inf:
+                    if dist[next_n] > dist[n] + next_d:
+                        dist[next_n] = dist[n] + next_d
+                        pre[next_n] = n
 
     #Check negative cycle
     for n in range(N):
-        for next_n in range(N):
-            if dist[next_n] > dist[n] + graph[n][next_n]:
-                return -1 #Negative cycle, 무한대로 줄어듬
-                break
+        for next_n, next_d in edge[n]:
+            if dist[n] != inf:
+                if dist[next_n] > dist[n] + next_d:
+                    return -1 #Negative cycle, 무한대로 줄어듬
+                    break
 
     return dist, pre
-print(bellmanford(graph,0))
+print(bellmanford(edge,0))
