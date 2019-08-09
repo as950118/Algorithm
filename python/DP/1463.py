508a5461509897b63ab6e4ed9a75e5e3229d827c
as950118 <na_qa@icloud.com>
2019-08-09 11:00:09 +0900

    algorithm

--- python/DP/1463.py
+++ python/DP/1463.py
@@ -1,17 +1,30 @@
 import sys
-sys.setrecursionlimit(10**8)
-inf = sys.maxsize
 
 n = int(input())
-dp = [inf for i in range(n+1)]
+dp = [0 for i in range(n+1)]
 dp[1] = 0
-def func(i):
-    if dp[i] != inf:
-        return dp[i]
-    dp[i] = func(i-1)+1
-    if i%2 == 0:
-        dp[i] = min(dp[i], func(i//2)+1)
-    if i%3 == 0:
-        dp[i] = min(dp[i], func(i//3)+1)
-    return dp[i]
-print(func(n))
+for i in range(2,n+1):
+    dp[i] = dp[i-1] + 1
+    if i%2==0:
+        temp = dp[i//2] + 1
+        if dp[i] > temp:
+            dp[i] = temp
+        #dp[i] = min(dp[i-1], dp[i//2]) + 1
+    if i%3 ==0:
+        temp = dp[i//3] + 1
+        if dp[i] > temp:
+            dp[i] = temp
+        #dp[i] = min(dp[i//3], dp[i-1], dp[i//2]) + 1
+print(dp[n])
+'''
+1      ->      n
+  2,3,4,5,6...
+2 -> 1+1 1x2
+3 -> 2+1 1x3
+4 -> 2x2 3+1
+5 -> 4+1
+.
+.
+.
+n -> n-1 +1 / n//2 * 2 / n//3 * 3
+'''
