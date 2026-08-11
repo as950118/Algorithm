1a16fc82e63a721e03eab8da731ef6b507403566
as950118 <na_qa@icloud.com>
2026-08-11 16:53:02 +0900

    Time: 7 ms (37.2%), Space: 19.9 MB (44%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,11 +1,9 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        max_prd = nums[0]
-        neg_max_prd = nums[0]
-        cur = max_prd
-        neg_cur = neg_max_prd
+        result = max_prd = min_prd = nums[0]
         for num in nums[1:]:
-            cur, neg_cur = max(num, cur*num, neg_cur*num), min(num, cur*num, neg_cur*num)
-            max_prd = max(max_prd, cur)
-            neg_max_prd = min(neg_max_prd, cur)
-        return max_prd
+            candidates = (num, max_prd * num, min_prd * num)
+            max_prd = max(candidates)
+            min_prd = min(candidates)
+            result = max(result, max_prd)
+        return result
