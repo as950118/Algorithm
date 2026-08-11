03b21badf3b3579e5553bcda675f6fbcf9f9f484
as950118 <na_qa@icloud.com>
2026-08-11 16:52:18 +0900

    Time: 16 ms (7.28%), Space: 20.2 MB (8.3%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -6,9 +6,7 @@ class Solution:
         neg_cur = neg_max_prd
         for num in nums[1:]:
             cur, neg_cur = max(num, cur*num, neg_cur*num), min(num, cur*num, neg_cur*num)
-            # cur = max(num, cur*num, neg_cur*num)
-            # neg_cur = min(num, cur*num, neg_cur*num)
             max_prd = max(max_prd, cur)
             neg_max_prd = min(neg_max_prd, cur)
             print(max_prd, neg_max_prd)
-        return max(max_prd, neg_max_prd)
+        return max_prd
