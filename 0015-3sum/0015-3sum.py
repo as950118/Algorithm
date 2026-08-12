8deff717d575f12b0bd097806b9666cc4f0c1a1c
as950118 <na_qa@icloud.com>
2026-08-12 23:20:25 +0900

    Time: 941 ms (16.96%), Space: 22.3 MB (53.87%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,32 +1,38 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        ret = []
         n = len(nums)
-        result = []
-
-        for i in range(n - 2):
-            # i 자리 중복 스킵
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            # 가장 작은 수가 0보다 크면 세 수 합이 절대 0이 될 수 없음
-            if nums[i] > 0:
+        nums.sort()
+        lo = 0
+        mid = lo+1
+        hi = n-1
+        while True:
+            if mid >= hi:
+                lo += 1
+                mid = lo+1
+                hi = n-1
+            if lo > n-3 or nums[lo] > 0:
                 break
+            if lo > 0 and nums[lo] == nums[lo - 1]:
+                lo += 1
+                mid = lo+1
+                hi = n-1
+                continue
+            cur = nums[lo] + nums[mid] + nums[hi]
+            if cur == 0:
+                ret.append([nums[lo], nums[mid], nums[hi]])
+                mid += 1
+                hi -= 1
+                while mid < hi and nums[mid] == nums[mid - 1]:
+                    mid += 1
+                while mid < hi and nums[hi] == nums[hi + 1]:
+                    hi -= 1
 
-            left, right = i + 1, n - 1
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
-                if total == 0:
-                    result.append([nums[i], nums[left], nums[right]])
-                    left += 1
-                    right -= 1
-                    # left, right 자리 중복 스킵
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-                elif total < 0:
-                    left += 1
-                else:
-                    right -= 1
+            if cur < 0:
+                mid += 1
+            elif cur > 0:
+                hi -= 1
 
-        return result
+            
+
+        return ret
