3045b696c861a1ffc85e9590a75b8c9f241975a4
as950118 <na_qa@icloud.com>
2026-08-14 21:23:42 +0900

    Time: 462 ms (74.27%), Space: 19.6 MB (78.48%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -2,31 +2,15 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         if amount == 0:
             return 0
-        coins.sort(reverse=True)
-        visit = {0: float('inf')}
-        def func(cur, count, coins):
-            if cur in visit:
-                if visit[cur] <= count:
-                    # print("HIT", cur, visit[cur], count)
-                    return
-            visit[cur] = count
+        n = len(coins)
+        dp = [float('inf')] * (amount+1)
+        dp[0] = 0
+        for i in range(amount+1):
             for coin in coins:
-                nxt = cur - coin
-                if nxt == 0:
-                    visit[0] = min(visit[0], count+1)
-                # else:
-                #     if nxt in visit:
-                #         visit[nxt] = min(visit[nxt], count)
-                #         func(nxt, visit[nxt]+1, coins)
-                elif nxt > 0:
-                    func(nxt, count+1 ,coins)
-        ret = func(amount, 0, coins)
-        print(visit)
-        if visit[0]:
-            if visit[0] == float('inf'):
-                return -1
-            return visit[0]
-        else:
+                cur = i + coin
+                if cur <= amount:
+                    dp[cur] = min(dp[cur], dp[i] + 1)
+        ret = dp[amount]
+        if ret == float('inf'):
             return -1
-        return visit[0]
-
+        return ret
