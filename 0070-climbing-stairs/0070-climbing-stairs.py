794f3daf0eede7982222556704ac6dc94765d275
as950118 <na_qa@icloud.com>
2026-08-14 09:03:51 +0900

    Time: 0 ms (100%), Space: 19.3 MB (52.29%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,18 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        visit = {}
+        memo = {0: 1, 1: 1}
         def climb(cur):
-            if cur in visit:
-                return visit[cur]
-            if cur > n:
-                visit[cur] = 0
-                return 0
-            if cur == n:
-                visit[cur] = 1
-                return 1
-            a = climb(cur+1)
-            b = climb(cur+2)
-            visit[cur+1] = a
-            visit[cur+2] = b
-            return a + b
-        return climb(1) + climb(2)
+            if cur in memo:
+                return memo[cur]
+            memo[cur] = climb(cur-1) + climb(cur-2)
+            return memo[cur]
+        return climb(n)
