0de3a7070ef4bba4f0357ca329d7598cc808004f
as950118 <na_qa@icloud.com>
2026-08-17 10:44:48 +0900

    Time: 7 ms (21.64%), Space: 20.5 MB (5.15%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,19 +1,24 @@
+from collections import deque
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         word_set = set(wordDict)
         n = len(s)
-        memo = {}
-
+        dp = [[None] * n for i in range(n)]
         def func(k):
             if k == n:
                 return True
-            if k in memo:
-                return memo[k]
-            for j in range(k, n):
-                if s[k:j+1] in word_set and func(j+1):
-                    memo[k] = True
-                    return True
-            memo[k] = False
-            return False
-
+            if dp[k][n-1] != None:
+                return dp[k][n-1]
+            for i in range(k, n):
+                if s[k:i+1] in word_set:
+                    ret = func(i+1)
+                    if ret:
+                        dp[k][i] = True
+                        return dp[k][i]            
+                else:
+                    dp[k][i] = False
+            if dp[0][k] == None:
+                dp[0][k] = False
+            return dp[0][k]
         return func(0)
+
