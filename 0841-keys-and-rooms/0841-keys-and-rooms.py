015fce2c293b98645ffb534844d5981d99e4411d
as950118 <na_qa@icloud.com>
2026-09-02 14:48:54 +0900

    Time: 0 ms (100%), Space: 19.6 MB (84.01%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -3,12 +3,11 @@ class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         keys = deque([0])
         visited = set()
-        stack = {num: room for num, room in enumerate(rooms)}
         while keys:
             key = keys.popleft()
             if key in visited:
                 continue
-            room = stack[key]
+            room = rooms[key]
             keys += room
             visited.add(key)
         if len(visited) == len(rooms):
